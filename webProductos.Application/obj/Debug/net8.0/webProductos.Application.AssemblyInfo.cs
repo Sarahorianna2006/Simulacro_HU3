744b2bf0827d236e8ec5d3d769cdee11fc316c07
sarahorianna2006 <soperez2006@gmail.com>
2025-11-12 11:44:39 -0500

    fix

--- webProductos.Application/obj/Debug/net8.0/webProductos.Application.AssemblyInfo.cs
+++ webProductos.Application/obj/Debug/net8.0/webProductos.Application.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("webProductos.Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0751bd40ffb377ccd9a266867015dbe23023eb55")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+89070989652aaaf57d221c71a632e954d265a064")]
 [assembly: System.Reflection.AssemblyProductAttribute("webProductos.Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("webProductos.Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
