97399309814ecb3c313501b4fe93482ccd8fc281
sarahorianna2006 <soperez2006@gmail.com>
2025-11-12 12:08:42 -0500

    commit

--- webProductos.Api/Program.cs
+++ webProductos.Api/Program.cs
@@ -133,6 +133,26 @@ using (var scope = app.Services.CreateScope())
     SeedAdmin.Seed(db);
 }
 
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "Product Catalog & User management API v1");
+        c.RoutePrefix = string.Empty;
+    });
+}
+
+if (app.Environment.IsDevelopment() || app.Environment.EnvironmentName == "Local")
+{
+    app.UseSwagger();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "Product Catalog API v1");
+        c.RoutePrefix = string.Empty;
+    });
+}
+
 app.Run();
 
 record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
